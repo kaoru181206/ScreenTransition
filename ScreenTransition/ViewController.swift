1be376f5c612c3384d4f1c98b450c4e178f3f543
kaoru.shiraga <karats24.kaoru@gmail.com>
2022-05-13 10:14:45 +0900

    Initial Commit

---
+++ ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by 白髪馨 on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
